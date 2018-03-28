6ebe6c178f0130eae2407c7dd2e8c175f7e231b5
OriStakhiv <orysiastakh@gmail.com>
2018-03-28 23:03:09 +0300

    new file with initialize method

---
+++ whack_a_ruby.rb
@@ -0,0 +1,20 @@
+class WhackARuby < Gosu::Window
+
+  def initialize
+    super(800, 600)
+    self.caption = "Whack the Ruby!"
+    @image = Gusu::Image.new('ruby.png')
+    @x = 200
+    @y = 200
+    @width = 50
+    @height = 43
+    @velocity_x = 5
+    @velocity_y = 5
+    @visible = 0
+    @hammer_image = Gosu::Image.new('hammer.png')
+    @hit = 0
+    @score = 0
+    @font = Gosu::Font.new(30)
+    @playing = true
+    @start_time = 0
+  end
